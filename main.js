2f4938f74f145d17a5ac5682da16d2954f851b85
Uschoolsaanvi <93886073+Uschoolsaanvi@users.noreply.github.com>
2021-11-07 15:43:33 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function changecolor1(){
+  document.getElementById("tag").style.color="white";
+}
+
+function changecolor(){
+  document.getElementById("para").style.color="white";
+   document.getElementById("tag2").style.color="white";
+ 
+}
